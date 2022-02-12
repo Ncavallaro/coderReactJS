9d0baf4c8efeffa2d13dc967700e5a6bb109ab1d
Ncavallaro <nicolascavallaro@live.com>
2022-02-12 00:30:30 -0300

    Se pasa por props una funcion al boton agregar al carrito

--- src/components/Body/ItemListContainer.js
+++ src/components/Body/ItemListContainer.js
@@ -1,12 +1,22 @@
 import ItemCount from "./ItemCount";
+//import ItemList from './ItemList';
+//import { useState } from 'react';
 
 const ItemListContainer = (props) => {
+
+  //const [count , setCount ] = useState("");
+  //const onAdd = (count) => setCount(count);
+  const onAdd = (cant) => {
+    console.log("Hola" + cant)
+}
+  //<ItemCount initial={1} stock={5} onclick={onAdd()} val={count} />
+
     return (
       <>
         <br />
         <h1>{props.titulo}</h1>
 
-        <ItemCount/>
+        <ItemCount stock={5} initial={1} clicked={() => onAdd()}/>
       </>
     );
   }
