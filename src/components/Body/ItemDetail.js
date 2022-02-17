187adc5b1931a002f67581765399e3ece956d07f
Ncavallaro <nicolascavallaro@live.com>
2022-02-17 16:09:48 -0300

    Se crean nuevos componentes para el detalle de un producto/viaje

---
+++ src/components/Body/ItemDetail.js
@@ -0,0 +1,40 @@
+import ItemCount from "./ItemCount";
+import '../../componentsCSS/Body/ItemDetail.css';
+
+const onAdd = (cant) => {
+    alert(cant)
+  }
+
+const ItemDetail = (props) => {
+    return(
+        <>
+            <article className='item'>
+                <div className="tituloCenter">
+                    <h3>{props.title}</h3>
+                </div>
+                <div className="imgDetail">
+                    <div> 
+                        <figure>
+                            <img src={props.img}/>
+                        </figure>
+                    </div>
+                    <div >
+                        <hr/>
+                            <p><b>Descripción: </b>{props.descrption}</p>
+                        <hr/>
+                            <p><b>Incluye: </b>{props.includ}</p>
+                            <h4>{"€ " + props.price}</h4>
+                        <hr/>
+                        <div class="inputStock">
+                            <input className="form-control" type="text" disabled value={"Stock " + props.stock + " unidades"} />
+                        </div>
+                    </div>
+                </div>
+        </article>
+
+        <ItemCount stock={5} initial={1} clicked={() => onAdd}/>
+        </>
+    )
+}
+
+export default ItemDetail;
